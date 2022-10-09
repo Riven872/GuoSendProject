316b948ac2e5be13dfc02dfd2128ba167fd644d2
riven <rivenzhou@rektec.com.cn>
2022-10-09 23:37:47 +0800

    代码提交

---
+++ GuoSend/src/main/java/com/Guo/GuoSend/common/MyMetaObjectHandler.java
@@ -0,0 +1,16 @@
+package com.Guo.GuoSend.common;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+
+public class MyMetaObjectHandler implements MetaObjectHandler {
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+
+    }
+}
